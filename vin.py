52e4cfa793cf51f796dadcbc0b1274b8f1376fbf
Nerissa Griffiths <ngriffit@villanova.edu>
2021-07-06 17:00:53 +0000

    added parsed data

--- vin.py
+++ vin.py
@@ -9,17 +9,32 @@ def get_auth():
       "authorization":'Basic {key}'.format(key=AUTH_KEY),
       "partner-token":TOKEN
     }
-    
+
     return headers
 
+
+def parse_data(r):
+    message = r['message']
+    data = r['data']
+    if (message['code'] == 0):
+        print(data['year'], data['make'], data['model'], data['manufacturer'],
+              data['engine'], data['trim'], data['transmission'])
+    else:
+        print('Invalid code')
+
 def main():
-    vin = input("Enter a Vehicle Identification Number: ")
+    vin = ''
+    while(len(vin) != 17):
+        vin = input("Enter a Vehicle Identification Number: ")
+        if(len(vin) != 17):
+            print('Please enter a valid VIN')
+    
     headers = get_auth()
     DECODE_URL = 'http://api.carmd.com/v3.0/decode?vin='
-    r = requests.get(DECODE_URL + vin, headers=headers)
-    
-    #print(r.statuscode)
-    print(r.json())
-    
+    r = requests.get(DECODE_URL + vin, headers=headers).json()
+    print(r)
+    parse_data(r)
+   
+
 if __name__ == "__main__":
     main()
