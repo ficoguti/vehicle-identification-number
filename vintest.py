77a9c088e9a154e0224b8bc91e885ee5230595f9
Nerissa Griffiths <ngriffit@villanova.edu>
2021-07-08 17:00:04 +0000

    added unittest and coverage test
    Co-authored-by: Federico Gutierrez <fedeguti2001@gmail.com>

--- vintest.py
+++ vintest.py
@@ -1,8 +1,11 @@
 import unittest
 import requests
-from vin import parse_data, get_auth, createDataFrame, loadDataset, createEngine
+from vin import parse_data, get_auth, createDataFrame, loadDataset
+from vin import createEngine
+
 
 DECODE_URL = 'http://api.carmd.com/v3.0/decode?vin='
+
 
 class TestFileName(unittest.TestCase):
     def test_parse_data(self):
@@ -17,12 +20,12 @@ class TestFileName(unittest.TestCase):
             "authorization": 'Basic ODYwYmMxNjQtNjE3OS00OGM5LWEwZGYtN2FkZTQ4ZjY0NmE3',
             "partner-token": 'cddae0cb72134c408a0836016130be55'
         }
-        
+
         self.assertEqual(get_auth(), headers)
 
         
     def test_createDataFrame(self):
-        r = {'data':{'year': 1999, 'make': 'FORD', 'model': 'TAURUS',
+        r = {'data': {'year': 1999, 'make': 'FORD', 'model': 'TAURUS',
              'manufacturer': 'FORD', 'engine': 'V6, 3.0L', 'trim': 'LX', 'transmission': 'AUTOMATIC'}}
         df = createDataFrame(r, '12345678912345678')
         self.assertFalse(df.empty)
@@ -35,7 +38,7 @@ class TestFileName(unittest.TestCase):
     def test_loadDataset(self):
         df = loadDataset('vindecoder', 'queries', 'vin-queries.sql')
         self.assertFalse(df.empty)
-        
-        
+
+
 if __name__ == '__main__':
     unittest.main()
